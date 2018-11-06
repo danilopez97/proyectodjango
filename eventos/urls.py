145046c8af6d552f780c08a5e08db3e43b08db14
danilopez97 <estu97dani@gmail.com>
2018-11-06 14:41:18 -0600

    se agregraron las views de tabla personas

--- eventos/urls.py
+++ eventos/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     url(r'^$', views.inicio, name='inicio'),
     url(r'^evento/(?P<pk>[0-9]+)/$', views.detalle_evento, name='detalle_evento'),
-   
     url(r'^evento/(?P<pk>[0-9]+)/editar/$', views.editar_evento, name='editar_evento'),
-    
-    
-    
     url(r'^evento/nuevo/$', views.evento_nuevo, name='evento_nuevo'),
     url(r'^evento/(?P<pk>\d+)/remove/$', views.evento_remove, name='evento_remove'),
-    
     url(r'^listar/evento/$', views.listar_eventos, name='listar_eventos'),
+    url(r'persona/(?P<pk>[0-9]+)/$', views.detalle_persona, name='detalle_persona'),
+    url(r'^persona/(?P<pk>[0-9]+)/editar/$', views.editar_persona, name='editar_persona'),
+    url(r'^listar/persona/$', views.listar_personas, name='listar_personas'),
+    url(r'^persona/nueva/$', views.persona_nueva, name='persona_nueva'),
+    url(r'^persona/(?P<pk>\d+)/remove/$', views.persona_remove, name='persona_remove'),
     ]
--- eventos/views.py
+++ eventos/views.py
@@ -4,14 +4,14 @@ from django.contrib import messages
 from .forms import EventoForm,PersonaForm
 from eventos.models import Persona, Evento
 from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LogoutView
-from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
 def detalle_evento(request, pk):
         post = get_object_or_404(Evento, pk=pk)
         return render(request, 'eventos/detalle_eventos.html', {'post': post})
+
+
 
 
 def listar_eventos(request):
@@ -20,12 +20,6 @@ def listar_eventos(request):
 
 def inicio(request):
     return render(request,'eventos/index.html')
-
-
-
-
-
-
 
 
 
@@ -57,6 +51,60 @@ def evento_remove(request, pk):
     post = get_object_or_404(Evento, pk=pk)
     post.delete()
     return redirect('listar_eventos')
+
+
+
+
+def listar_personas(request):
+        posts=Persona.objects.all()
+        return render(request, 'eventos/listarpersonas.html', {'posts':posts})
+
+def detalle_persona(request, pk):
+        post = get_object_or_404(Persona, pk=pk)
+        return render(request, 'eventos/detalle_personas.html', {'post': post})
+
+def persona_nueva(request):
+    if request.method == "POST":
+        formulario = PersonaForm(request.POST,request.FILES)
+        if formulario.is_valid():
+            add=formulario.save(commit=False)
+            add.save()
+            formulario.save_m2m()
+            messages.add_message(request, messages.SUCCESS, 'Evento Guardado Exitosamente')
+            return redirect('detalle_persona', pk=add.pk)    
+    else:
+
+        formulario = PersonaForm()
+
+    return render(request, 'eventos/ingreso_persona.html', {'formulario': formulario})
+
+def editar_persona(request, pk):
+    prod = get_object_or_404(Persona, pk=pk)
+
+    if request.method == "POST":
+
+        form = PersonaForm(request.POST,request.FILES, instance=prod)
+        if form.is_valid():
+            edit_persona=form.save(commit=False)
+            form.save_m2m()
+            edit_persona.save()
+            return redirect('detalle_persona', pk=edit_persona.pk)
+
+            #evento = Evento.objects.create(nombre=formulario.cleaned_data['nombre'], fecha = formulario.cleaned_data['fecha'])
+            
+
+           #messages.add_message(request, messages.SUCCESS, 'Evento Guardado Exitosamente')
+
+            #evento = Evento.objects.create(nombre=form.cleaned_data['nombre'], fecha = form.cleaned_data['fecha'])
+
+    else:
+        form = PersonaForm(instance=prod)
+    return render(request, 'eventos/editar_persona.html', {'form': form})
+
+def persona_remove(request, pk):
+    post = get_object_or_404(Persona, pk=pk)
+    post.delete()
+    return redirect('listar_personas')
     
 
         
